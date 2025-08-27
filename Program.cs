5f42e3de70928f38ae6ba4332727894738cceb6f
Ameen shalabi <ameen.ammar3933@gmail.com>
2025-08-27 21:08:18 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace gitproject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
