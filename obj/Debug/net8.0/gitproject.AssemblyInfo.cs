41a3524088f277a12c28f2ad03636d7c015bf19b
Ameen shalabi <ameen.ammar3933@gmail.com>
2025-08-30 03:52:32 +0300

    yes

--- obj/Debug/net8.0/gitproject.AssemblyInfo.cs
+++ obj/Debug/net8.0/gitproject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("gitproject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6f1c935c37b217c0f3eedfd328c61401bf5f43e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+adaddf9ac833f91d4e0ae302f5ad3f531402b1ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("gitproject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("gitproject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
